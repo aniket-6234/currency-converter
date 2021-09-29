16950c3b644040709244ea1a2606e6084153a548
aniket-6234 <aniketashok1999@gmail.com>
2021-09-29 17:57:24 +0530

    Heading

---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div className="circle">
+        <div className="red"></div>
+        <div className="blue"></div>
+        <div className="green"></div>
+      </div>
+      <h1 className="head-1">CURRENCY</h1>
+      <h1 className="head-2">CONVERTER</h1>
+      <div className="line"></div>
+    </nav>
+  )
+}
+
+export default Navbar
